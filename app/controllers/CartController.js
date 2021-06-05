976097295142ea68e8e40d79640dfdcae358933a
unknown <dakidhem@gmail.com>
2021-06-05 19:56:53 +0100

    5 juin

--- app/controllers/CartController.js
+++ app/controllers/CartController.js
@@ -1,6 +1,6 @@
-const httpStatus = require('http-status');
-const APIError = require('../helpers/APIError');
-const Cart = require('../models/Cart');
+const httpStatus = require("http-status");
+const APIError = require("../helpers/APIError");
+const Cart = require("../models/Cart");
 
 /**
  * add product to cart.
@@ -12,14 +12,14 @@ const Cart = require('../models/Cart');
 exports.add = function (req, res, next) {
   const { email, product_id } = req.body;
   const qty = Number.parseInt(req.body.qty);
-  console.log('qty: ', qty);
+  console.log("qty: ", qty);
   Cart.findOne({ email: email })
     .exec()
-    .then(cart => {
+    .then((cart) => {
       if (!cart && qty <= 0) {
-        throw new Error('Invalid request');
+        throw new Error("Invalid request");
       } else if (cart) {
-        const indexFound = cart.items.findIndex(item => {
+        const indexFound = cart.items.findIndex((item) => {
           return item.product_id === product_id;
         });
         if (indexFound !== -1 && qty <= 0) {
@@ -29,10 +29,10 @@ exports.add = function (req, res, next) {
         } else if (qty > 0) {
           cart.items.push({
             product_id: product_id,
-            qty: qty
+            qty: qty,
           });
         } else {
-          throw new Error('Invalid request');
+          throw new Error("Invalid request");
         }
         return cart.save();
       } else {
@@ -41,18 +41,18 @@ exports.add = function (req, res, next) {
           items: [
             {
               product_id: product_id,
-              qty: qty
-            }
-          ]
+              qty: qty,
+            },
+          ],
         };
         cart = new Cart(cartData);
         return cart.save();
       }
     })
-    .then(savedCart => res.json(savedCart))
-    .catch(err => {
+    .then((savedCart) => res.json(savedCart))
+    .catch((err) => {
       let error;
-      if (err.message === 'Invalid request') {
+      if (err.message === "Invalid request") {
         error = new APIError(err.message, httpStatus.BAD_REQUEST, true);
       } else {
         error = new APIError(err.message, httpStatus.NOT_FOUND);
@@ -71,36 +71,36 @@ exports.add = function (req, res, next) {
 exports.subtract = function (req, res, next) {
   const { email, product_id } = req.body;
   const qty = Number.parseInt(req.body.qty);
-  console.log('qty: ', qty);
+  console.log("qty: ", qty);
   Cart.findOne({ email: email })
     .exec()
-    .then(cart => {
+    .then((cart) => {
       if (!cart || qty <= 0) {
-        throw new Error('Invalid request');
+        throw new Error("Invalid request");
       } else {
-        const indexFound = cart.items.findIndex(item => {
+        const indexFound = cart.items.findIndex((item) => {
           return item.product_id === product_id;
         });
         if (indexFound !== -1) {
-          console.log('index Found: ', indexFound);
-          console.log('before update items: ', cart.items);
+          console.log("index Found: ", indexFound);
+          console.log("before update items: ", cart.items);
           let updatedQty = cart.items[indexFound].qty - qty;
           if (updatedQty <= 0) {
             cart.items.splice(indexFound, 1);
           } else {
             cart.items[indexFound].qty = updatedQty;
           }
-          console.log('after update items: ', cart.items);
+          console.log("after update items: ", cart.items);
           return cart.save();
         } else {
-          throw new Error('Invalid request');
+          throw new Error("Invalid request");
         }
       }
     })
-    .then(updatedCart => res.json(updatedCart))
-    .catch(err => {
+    .then((updatedCart) => res.json(updatedCart))
+    .catch((err) => {
       let error;
-      if (err.message === 'Invalid request') {
+      if (err.message === "Invalid request") {
         error = new APIError(err.message, httpStatus.BAD_REQUEST, true);
       } else {
         error = new APIError(err.message, httpStatus.NOT_FOUND);
@@ -118,13 +118,17 @@ exports.get = function (req, res, next) {
   const { email } = req.query;
   // console.log(email);
   if (!email) {
-    const error = new APIError('Invalid request', httpStatus.BAD_REQUEST, true);
+    const error = new APIError("Invalid request", httpStatus.BAD_REQUEST, true);
     return next(error);
   }
   Cart.get({ email })
-    .then(Cart => res.json(Cart))
-    .catch(err => {
-      const error = new APIError(err.message, httpStatus.INTERNAL_SERVER_ERROR, true);
+    .then((Cart) => res.json(Cart))
+    .catch((err) => {
+      const error = new APIError(
+        err.message,
+        httpStatus.INTERNAL_SERVER_ERROR,
+        true
+      );
       return next(error);
     });
 };
@@ -137,10 +141,41 @@ exports.get = function (req, res, next) {
 exports.remove = function (req, res, next) {
   const { email } = req.query;
   Cart.get({ email })
-    .then(Cart => Cart.remove())
-    .then(deletedCart => res.json(deletedCart))
-    .catch(err => {
-      const error = new APIError(err.message, httpStatus.INTERNAL_SERVER_ERROR, true);
+    .then((Cart) => Cart.remove())
+    .then((deletedCart) => res.json(deletedCart))
+    .catch((err) => {
+      const error = new APIError(
+        err.message,
+        httpStatus.INTERNAL_SERVER_ERROR,
+        true
+      );
       return next(error);
+    });
+};
+/**
+ * delete cart by email.
+ * @property {string} req.query.email
+ * @returns {Cart}
+ */
+exports.deleteProduit = (req, res) => {
+  const id = req.params.id;
+
+  cart.items
+    .findByIdAndRemove(id, { useFindAndModify: false })
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: `Impossible de supprimer le produit avec l'identifiant=${id}. Peut-être que le produit n'a pas été trouvé!`,
+        });
+      } else {
+        res.send({
+          message: "Le produit a été supprimé avec succès!",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Impossible de supprimer le produit avec l'identifiant=" + id,
+      });
     });
 };
