501053ebbffffd4e57a9f3c83af8c0d9d0caac44
zaki1003 <mz.merabet@esi-sba.dz>
2021-09-07 22:58:06 +0100

    adding changes

--- app/routes/cart.routes.js
+++ app/routes/cart.routes.js
@@ -1,6 +1,6 @@
 module.exports = (app) => {
   const CartController = require("../controllers/CartController.js");
-  var router = require("express").Router();
+  var router = require("express").Router();aa
 
   router.get("/cart", CartController.get);
   router.post("/addCart", CartController.add);
